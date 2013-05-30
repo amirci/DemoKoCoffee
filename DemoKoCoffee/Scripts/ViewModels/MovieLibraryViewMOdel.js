8f7e41ebcdea589a02985ad30ae34f14645ad3cd
Amir Barylko <amir@barylko.com>
2013-05-30 03:46:40 -0600

    Added form to add movies inline

--- DemoKoCoffee/Scripts/ViewModels/MovieLibraryViewMOdel.js
+++ DemoKoCoffee/Scripts/ViewModels/MovieLibraryViewMOdel.js
@@ -1,5 +1,5 @@
 (function() {
-  var Movie,
+  var Movie, NewMovieViewModel,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   DemoKoCoffee.MovieLibraryViewModel = (function() {
@@ -7,16 +7,22 @@
       this.cancelEditing = __bind(this.cancelEditing, this);
       this.saveEditing = __bind(this.saveEditing, this);
       this.editTitle = __bind(this.editTitle, this);
+      this.newMovie = __bind(this.newMovie, this);
       var _this = this;
 
       this.title = ko.observable(title);
-      this.movies = [new Movie('Blazing Saddles', new Date(1972, 1, 3)), new Movie('Young Frankenstain', new Date(1972, 1, 5)), new Movie('Spaceballs', new Date(1980, 1, 3))];
+      this.movies = ko.observableArray([new Movie('Blazing Saddles', new Date(1972, 1, 3)), new Movie('Young Frankenstain', new Date(1972, 1, 5)), new Movie('Spaceballs', new Date(1980, 1, 3))]);
       this.editingTitle = ko.observable(false);
       this.newTitle = ko.observable();
       this.count = ko.computed(function() {
-        return _this.movies.length;
+        return _this.movies().length;
       });
+      this.newMovieVM = new NewMovieViewModel(this.movies);
     }
+
+    MovieLibraryViewModel.prototype.newMovie = function() {
+      return this.newMovieVM.active(true);
+    };
 
     MovieLibraryViewModel.prototype.editTitle = function() {
       this.newTitle(this.title());
@@ -36,10 +42,33 @@
 
   })();
 
+  NewMovieViewModel = (function() {
+    function NewMovieViewModel(movies) {
+      this.movies = movies;
+      this.cancel = __bind(this.cancel, this);
+      this.save = __bind(this.save, this);
+      this.active = ko.observable(false);
+      this.title = '';
+      this.relDate = '';
+    }
+
+    NewMovieViewModel.prototype.save = function() {
+      this.active(false);
+      return this.movies.push(new Movie(this.title, new Date(this.relDate)));
+    };
+
+    NewMovieViewModel.prototype.cancel = function() {
+      return this.active(false);
+    };
+
+    return NewMovieViewModel;
+
+  })();
+
   Movie = (function() {
     function Movie(title, relDate) {
-      this.title = title;
-      this.relDate = relDate;
+      this.title = title != null ? title : '';
+      this.relDate = relDate != null ? relDate : null;
       this.releaseDate = $.format.date(this.relDate, 'MMM dd yyyy');
     }
 
--- DemoKoCoffee/Scripts/ViewModels/MovieLibraryViewModel.coffee
+++ DemoKoCoffee/Scripts/ViewModels/MovieLibraryViewModel.coffee
@@ -2,14 +2,17 @@ class DemoKoCoffee.MovieLibraryViewModel
   
   constructor: (title) ->
     @title = ko.observable(title)
-    @movies = [
+    @movies = ko.observableArray [
       new Movie('Blazing Saddles', new Date(1972, 1, 3)),
       new Movie('Young Frankenstain', new Date(1972, 1, 5)),
       new Movie('Spaceballs', new Date(1980, 1, 3)),
     ]
     @editingTitle = ko.observable(false)
     @newTitle = ko.observable()
-    @count = ko.computed => @movies.length
+    @count = ko.computed => @movies().length
+    @newMovieVM = new NewMovieViewModel(@movies)
+
+  newMovie: => @newMovieVM.active(true)
     
   editTitle: => 
     @newTitle @title()
@@ -21,8 +24,20 @@ class DemoKoCoffee.MovieLibraryViewModel
   
   cancelEditing: => @editingTitle(false)
 
+class NewMovieViewModel
+
+  constructor: (@movies) ->
+    @active = ko.observable(false)
+    @title = ''
+    @relDate = ''
+    
+  save: =>
+    @active(false)
+    @movies.push new Movie(@title, new Date @relDate)
+    
+  cancel: => @active(false)
     
 class Movie
-  
-  constructor: (@title, @relDate) ->
+
+  constructor: (@title='', @relDate=null) ->
     @releaseDate = $.format.date @relDate, 'MMM dd yyyy'
