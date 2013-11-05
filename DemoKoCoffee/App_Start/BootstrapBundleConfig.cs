05b69d27f4c86986bf94bbb0058c2281a37dd8f6
Amir Barylko <amir@barylko.com>
2013-11-05 15:52:12 -0600

    Added ko.mapping, movie controller serving json and the mapping from the json to the movie model

--- DemoKoCoffee/App_Start/BootstrapBundleConfig.cs
+++ DemoKoCoffee/App_Start/BootstrapBundleConfig.cs
@@ -8,11 +8,7 @@ namespace DemoKoCoffee
         {
             bundles.Add(new ScriptBundle("~/js").Include(
                 "~/Scripts/jquery-{version}.js",
-                "~/Scripts/jquery-migrate-{version}.js",
                 "~/Scripts/bootstrap.js",
-                "~/Scripts/jquery.validate.js",
-                "~/scripts/jquery.validate.unobtrusive.js",
-                "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js",
                 "~/Scripts/knockout.2.2.0.js",
                 "~/Scripts/ViewModels/DemoKoCoffee.js",
                 "~/Scripts/ViewModels/MovieLibraryViewModels.js"
