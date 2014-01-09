64e8d579a3781838b931a3678391dc8949679e96
Amir Barylko <amir@barylko.com>
2014-01-09 13:36:49 -0600

    Changed index method to use mongodb storage

--- DemoKoCoffee/Controllers/MoviesController.cs
+++ DemoKoCoffee/Controllers/MoviesController.cs
@@ -1,5 +1,8 @@
-﻿using System;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using WebGrease.Css.Extensions;
 
 namespace DemoKoCoffee.Controllers
 {
@@ -9,17 +12,46 @@ namespace DemoKoCoffee.Controllers
         // GET: /Movies/
         public ActionResult Index()
         {
-            var result = new
+            const string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            var library = server.GetDatabase("MovieLibrary");
+            var repository = library.GetCollection<Movie>("movies");
+
+            if (repository.Count() == 0)
             {
-                movies = new[]
-                    {
-                        new {title = "Blazing Saddles", releaseDate = "Mar 1, 1972"},
-                        new {title = "Young Frankenstain", releaseDate = "Jan 1, 1972"},
-                        new {title = "Spaceballs", releaseDate = "Mar 3, 1980"}
-                    }
-            };
-            return Json(result, JsonRequestBehavior.AllowGet);
+                PopulateDefaultMovies(repository);
+            }
+
+            var movies = repository.FindAll();
+
+            var result =
+              JsonConvert.SerializeObject(
+                new { movies },
+                Formatting.Indented,
+                new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }
+              );
+
+            return this.Content(result, "application/json");
         }
 
+        private static void PopulateDefaultMovies(MongoCollection<Movie> repository)
+        {
+            var movies = new[]
+            {
+                new Movie {Id = 1, Title = "Blazing Saddles", ReleaseDate = "Mar 1, 1972"},
+                new Movie {Id = 2, Title = "Young Frankenstain", ReleaseDate = "Jan 1, 1972"},
+                new Movie {Id = 3, Title = "Spaceballs", ReleaseDate = "Mar 3, 1980"}
+            };
+
+            movies.ForEach(m => repository.Save(m));
+        }
+    }
+
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string ReleaseDate { get; set; }
+        public int Id { get; set; }
     }
 }
