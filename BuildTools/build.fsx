ce0cd4437239820b8be1fd604dee07a68ea33a87
Amir Barylko <amir@barylko.com>
2015-09-03 18:01:55 -0400

    Added fake build

---
+++ BuildTools/build.fsx
@@ -0,0 +1,32 @@
+﻿// include Fake lib
+#r @"../packages/FAKE/tools/FakeLib.dll"
+
+open System.IO
+open Fake
+
+RestorePackages()
+
+#load "./config.fsx"
+
+open Config
+
+Target "Help" PrintTargets
+
+let addBuildTarget name env sln =
+    let rebuild config = {(setParams config) with Targets = ["Rebuild"]}
+    Target (targetWithEnv name env) (fun _ ->
+        setBuildMode env
+        build rebuild sln
+    )
+
+environments |> Seq.iter (fun env -> 
+    addBuildTarget prjName env mainSln
+
+    Target  (targetWithEnv "All" env) (fun _ ->
+        run (targetWithEnv prjName env)
+    )
+)
+
+
+// start build
+RunTargetOrDefault "All:Debug"
